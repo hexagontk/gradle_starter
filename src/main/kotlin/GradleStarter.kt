06a23ba24438910302ddd50393bbd78f1cfc1a97
Juanjo Aguililla <aguilillaj@ryanair.com>
2019-01-31 21:20:36 +0100

    Update starter

--- src/main/kotlin/GradleStarter.kt
+++ src/main/kotlin/GradleStarter.kt
@@ -2,24 +2,31 @@ package org.example
 
 import java.time.LocalDateTime.now
 
-import com.hexagonkt.server.*
-import com.hexagonkt.server.jetty.JettyServletAdapter
-import com.hexagonkt.settings.SettingsManager
+import com.hexagonkt.http.httpDate
+import com.hexagonkt.http.server.*
+import com.hexagonkt.http.server.jetty.JettyServletAdapter
+import com.hexagonkt.injection.InjectionManager.bindObject
+import com.hexagonkt.injection.InjectionManager.inject
 
-val server: Server = server(JettyServletAdapter(), SettingsManager.settings) {
-    before {
-        response.addHeader("Server", "Servlet/3.1")
-        response.addHeader("Transfer-Encoding", "chunked")
-        response.addHeader("Date", httpDate(now()))
+val server: Server by lazy {
+    Server {
+        before {
+            response.addHeader("Server", "Servlet/3.1")
+            response.addHeader("Transfer-Encoding", "chunked")
+            response.addHeader("Date", httpDate(now()))
+        }
+
+        get("/text") { ok("Hello, World!", "text/plain") }
     }
-
-    get("/text") { ok("Hello, World!", "text/plain") }
 }
+
+private fun Server(block: Router.() -> Unit): Server = Server(inject(), block = block)
 
 /**
  * Start the service from the command line.
  */
 fun main() {
+    bindObject<ServerPort>(JettyServletAdapter())
     server.run()
 }
 
--- src/test/kotlin/GradleStarterTest.kt
+++ src/test/kotlin/GradleStarterTest.kt
@@ -1,11 +1,12 @@
 package org.example
 
-import com.hexagonkt.client.Client
+import com.hexagonkt.http.client.Client
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class GradleStarterTest {
+
     private val client by lazy { Client("http://localhost:${server.runtimePort}") }
 
     @Before fun startup() {
@@ -16,14 +17,14 @@ class GradleStarterTest {
         server.stop()
     }
 
-    @Test fun httpRequest() {
+    @Test fun `HTTP request returns proper status, headers and body`() {
         val response = client.get("/text")
         val content = response.responseBody
 
-        assert(response.headers ["Date"] != null)
-        assert(response.headers ["Server"] != null)
-        assert(response.headers ["Transfer-Encoding"] != null)
-        assert(response.headers ["Content-Type"] == "text/plain")
+        assert(response.headers["Date"] != null)
+        assert(response.headers["Server"] != null)
+        assert(response.headers["Transfer-Encoding"] != null)
+        assert(response.headers["Content-Type"] == "text/plain")
 
         assert("Hello, World!" == content)
     }
