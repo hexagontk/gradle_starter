f5c6b8fdf7fce699e6037990abed3380219a2c09
Juanjo Aguililla <juanjoaguililla@gmail.com>
2019-02-18 20:31:43 +0100

    Update starter template

--- src/main/kotlin/GradleStarter.kt
+++ src/main/kotlin/GradleStarter.kt
@@ -6,21 +6,18 @@ import com.hexagonkt.http.httpDate
 import com.hexagonkt.http.server.*
 import com.hexagonkt.http.server.jetty.JettyServletAdapter
 import com.hexagonkt.injection.InjectionManager.bindObject
-import com.hexagonkt.injection.InjectionManager.inject
 
 val server: Server by lazy {
     Server {
         before {
-            response.addHeader("Server", "Servlet/3.1")
-            response.addHeader("Transfer-Encoding", "chunked")
-            response.addHeader("Date", httpDate(now()))
+            response.setHeader("Server", "Servlet/3.1")
+            response.setHeader("Transfer-Encoding", "chunked")
+            response.setHeader("Date", httpDate(now()))
         }
 
         get("/text") { ok("Hello, World!", "text/plain") }
     }
 }
-
-private fun Server(block: Router.() -> Unit): Server = Server(inject(), block = block)
 
 /**
  * Start the service from the command line.
