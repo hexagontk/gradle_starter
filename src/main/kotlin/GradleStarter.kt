b6270e658e91f5f7809830f8a0027a6f8124f0ab
jaguililla <juanjoaguililla@gmail.com>
2022-02-18 21:01:04 +0100

    Fix test

--- src/main/kotlin/GradleStarter.kt
+++ src/main/kotlin/GradleStarter.kt
@@ -3,7 +3,6 @@ package org.example
 import com.hexagonkt.http.server.*
 import com.hexagonkt.http.server.netty.NettyServerAdapter
 import com.hexagonkt.core.logging.LoggingManager
-import com.hexagonkt.core.logging.info
 import com.hexagonkt.core.media.TextMedia.PLAIN
 import com.hexagonkt.http.model.ContentType
 import com.hexagonkt.logging.slf4j.jul.Slf4jJulLoggingAdapter
@@ -24,6 +23,5 @@ internal val server: HttpServer by lazy {
 
 internal fun main() {
     LoggingManager.adapter = Slf4jJulLoggingAdapter()
-    URL("classpath:META-INF/build.properties").readText().info("Build Properties\n")
     server.start()
 }
