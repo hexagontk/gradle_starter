97e0c847341e6eedc64d2552b5be0a11fb55173c
jaguililla <juanjoaguililla@gmail.com>
2022-10-27 10:26:49 +0200

    Update Hexagon version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.graalvm.buildtools.gradle.dsl.GraalVMExtension
-import proguard.gradle.ProGuardTask
 
 plugins {
     kotlin("jvm") version("1.7.20")
@@ -7,50 +5,15 @@ plugins {
     id("org.graalvm.buildtools.native") version("0.9.16")
 }
 
-buildscript {
-    dependencies {
-        classpath("com.guardsquare:proguard-gradle:7.2.2")
-    }
-}
-
 val gradleScripts = properties["gradleScripts"]
 val hexagonVersion = properties["hexagonVersion"]
 
 apply(from = "$gradleScripts/kotlin.gradle")
 apply(from = "$gradleScripts/application.gradle")
+apply(from = "$gradleScripts/native.gradle")
 
 extensions.configure<JavaApplication> {
     mainClass.set("org.example.GradleStarterKt")
-}
-
-// TODO Check: https://www.guardsquare.com/manual/setup/gradle
-tasks.register<ProGuardTask>("proguard") {
-    dependsOn("jarAll")
-
-    injars(file("build/libs/${project.name}-all-${project.version}.jar"))
-    outjars(file("build/libs/${project.name}-proguard-${project.version}.jar"))
-
-    val filter = mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class")
-    libraryjars(filter, "${System.getProperty("java.home")}/jmods/java.base.jmod")
-    libraryjars(filter, "${System.getProperty("java.home")}/jmods/java.logging.jmod")
-    libraryjars(filter, "${System.getProperty("java.home")}/jmods/java.management.jmod")
-    libraryjars(filter, "${System.getProperty("java.home")}/jmods/java.xml.jmod")
-    libraryjars(filter, "${System.getProperty("java.home")}/jmods/jdk.unsupported.jmod")
-
-    dontnote("kotlin.**")
-    dontnote("kotlinx.**")
-
-    keepkotlinmetadata()
-
-    dontwarn()
-    dontoptimize()
-
-    keep("""class org.example.GradleStarterKt {
-        |   *;
-        |}
-    """.trimMargin())
-    keep("class com.hexagonkt.**")
-    keep("class io.netty.**")
 }
 
 dependencies {
@@ -58,35 +21,4 @@ dependencies {
     "implementation"("com.hexagonkt:logging_slf4j_jul:$hexagonVersion")
 
     "testImplementation"("com.hexagonkt:http_client_jetty:$hexagonVersion")
-}
-
-graalvmNative {
-    metadataRepository {
-        enabled.set(true)
-    }
-}
-
-//extensions.configure<GraalVMReachabilityMetadataRepositoryExtension> {
-//    enabled.set(true)
-//}
-
-extensions.configure<GraalVMExtension> {
-    binaries {
-        named("main") {
-            listOf(
-                "--enable-http",
-                "--enable-https",
-                "--enable-url-protocols=classpath",
-                "--initialize-at-build-time=com.hexagonkt.core.ClasspathHandler",
-            )
-            .forEach(buildArgs::add)
-        }
-    }
-}
-
-tasks.register<Exec>("upx") {
-    dependsOn("nativeCompile")
-
-    val command = "upx $buildDir/native/nativeCompile/${project.name} -o $buildDir/${project.name}"
-    commandLine(command.split(" "))
 }
